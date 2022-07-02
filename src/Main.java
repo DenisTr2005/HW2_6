173218c0a15d17d37661835cdf7634bacd242ac4
Денис <den_2005@list.ru>
2022-07-02 13:29:39 +0300

    commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,45 @@
 import java.util.*;
 
 public class Main {
+    public  static List<Integer> nums = new ArrayList<>(List.of(35, 56, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    public  static List<String> words = new ArrayList<>(List.of("Слово", "Собака", "Кошка", "Кошка",
+            "Word", "Word3", "Кошка", "Word3"));
+    public static String string = "Это новый текст о том как из предложения сделать список слов";
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(35, 56, 1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         System.out.println(nums);
+        System.out.println(words + "\n");
+        numsOdd();
+        numsEven();
+        wordsSet();
+        wordsRepeat();
+        System.out.println(stringToList(string));
+    }
+    public static void numsOdd() {
         List<Integer> numsOdd = new ArrayList<>();
-        for (Integer num : nums) {
+        for (int num : nums) {
             if (num % 2 == 1) {
                 numsOdd.add(num);
             }
         }
-        System.out.println(numsOdd);
-
-        //Задание 2
+        System.out.println("Задание 1. Нечетные числа в списке:\n" + numsOdd);
+    }
+    public static void numsEven() {
         List<Integer> numsEven = new ArrayList<>();
-        for (Integer num : nums) {
+        for (int num : nums) {
             if (num % 2 == 0 && !numsEven.contains(num)) {
                 numsEven.add(num);
             }
         }
-        numsEven.sort(Integer::compareTo);
-        System.out.println(numsEven);
-
-        //Задание 3
-        List<String> words = new ArrayList<>(List.of("Слово", "Собака", "Кошка","Кошка",
-                                                        "Word", "Word3","Кошка", "Word3"));
-        System.out.println(words);
-        Set<String> wordsSet = new HashSet<>();
-        for (String word : words) {
-            wordsSet.add(word);
-        }
-        System.out.println(wordsSet);
-
-        //Задание 4
+        Collections.sort(numsEven);
+        System.out.println("Задание 2. Четные уникальные отсортированные числа в списке:\n" + numsEven);
+    }
+    public static void wordsSet() {
+        System.out.println("Задание 3. Уникальные слова в списке:\n" + new HashSet<>(words));
+    }
+    public static void wordsRepeat() {
         Map<String, Integer> wordsRepeat = new HashMap<>();
-        Integer repeat = 0;
-        for (String word : wordsSet) {
+        int repeat = 0;
+        for (String word : new HashSet<>(words)) {
             for (String s : words) {
                 if (s.equals(word)) {
                     repeat++;
@@ -46,6 +50,9 @@ public class Main {
             }
             repeat = 0;
         }
-        System.out.println(wordsRepeat);
+        System.out.println("Задание 4. Число дублей в списке:\n" + wordsRepeat);
+    }
+    public static ArrayList<String> stringToList(String s) {
+        return new ArrayList<>(List.of(s.split(" ")));
     }
 }
